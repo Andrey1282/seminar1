dbd030b308c38ae846f9ea212817763783477c24
Andrey Mikheev <athlon26@mail.ru>
2025-03-05 13:14:15 +0300

    add dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Answer {
+static void CheckDivisibility(int firstNumber, int secondNumber)
+{
+    Console.Write("Введите целое число: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите целое число: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    int result=num1%num2
+        if (result==0)
+        {
+            Console.WriteLine("делится");
+        }
+    else
+    {
+        Console.WriteLine("не делится");
+    }
+}
+static public void Main(string[] args) {
+int firstNumber, secondNumber;
+if (args.Length >= 2) {
+firstNumber = int.Parse(args[0]);
+secondNumber = int.Parse(args[1]);
+} else {
+firstNumber = 10;
+secondNumber = 2;
+}
+CheckDivisibility(firstNumber, secondNumber);
+}
+}
